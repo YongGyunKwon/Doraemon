64da0325eacb1329e216594d513656c646e5394c
YongGyunKwon <ygkwon50@gmail.com>
2019-06-09 14:37:24 +0900

    e

---
+++ doraemon/WebContent/testconnect.jsp
@@ -0,0 +1,42 @@
+<%@ page language="java" contentType="text/html; charset=EUC-KR"
+    pageEncoding="EUC-KR"%>
+
+<%@page import = "java.sql.*" %>
+    
+<%
+   Connection conn = null;                                        // null로 초기화 한다.
+
+   try{
+
+   String url="jdbc:mysql://localhost:3306/doraemon";
+      
+   String id = "root";                                                    // 사용자 계정
+
+   String pw = "110100";                                                // 사용자 계정의 패스워드
+
+
+
+   Class.forName("com.mysql.jdbc.Driver");                       // 데이터베이스와 연동하기 위해 DriverManager에 등록한다
+   
+   conn=DriverManager.getConnection(url,id,pw);              // DriverManager 객체로부터 Connection 객체를 얻어온다.
+
+   out.println("제대로 연결되었습니다.");                            // 커넥션이 제대로 연결되면 수행된다.
+	
+   }catch(Exception e){                                                    // 예외가 발생하면 예외 상황을 처리한다.
+
+   e.printStackTrace();
+   out.println("연결실패");
+   }   
+
+%>
+    
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="EUC-KR">
+<title>Insert title here</title>
+</head>
+<body>
+
+</body>
+</html>
---
+++ doraemon/src/controller/UserRegisterController.java
@@ -0,0 +1,63 @@
+package controller;
+
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import controller.Controller;
+import controller.HttpUtil;
+import service.UserService;
+import vo.UserVo;
+
+
+public class UserRegisterController implements Controller {
+
+	@Override
+	public void execute(HttpServletRequest req,HttpServletResponse res) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+
+		req.setCharacterEncoding("UTF-8");
+		res.setContentType("text/html; charSet = UTF-8");
+		
+		UserService service = UserService.getInstance();
+		int result = 0;
+		SimpleDateFormat format = new SimpleDateFormat("yyyy-mm-dd");
+		
+		String name= req.getParameter("name");
+		String email = req.getParameter("email");
+		String password=req.getParameter("password");
+		
+		UserVo user=new UserVo();
+	    user.setName(name);
+	    user.setEmail(email);
+	    user.setPassword(password);
+	    /*
+	     if(service.selectUser(user) == null)
+	     {    	 
+	    	 if(service.selectUser_byPhone_Number(user) == null)
+	    	 {
+	    		 service.userCreate(user);
+	        	 HttpUtil.forward(req, res,"login.jsp");
+	        	 return;
+	    	 }
+	    	 else
+	    	 {
+	    		 System.out.println("동일한 회원 핸드폰 번호 존재");
+	    		 HttpUtil.forward(req, res,"register.jsp");
+	    	 }
+	     }
+	     
+	     else {
+	    	 System.out.println("동일한 이메일 존재");
+	    	 HttpUtil.forward(req, res,"register.jsp");
+	     }*/
+	    service.userCreate(user);
+	    HttpUtil.forward(req, res,"login.jsp");
+	    
+	    return;
+	}
+}
